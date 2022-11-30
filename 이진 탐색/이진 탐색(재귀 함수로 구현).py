b0903f817b05457109e5b0f5f5b1c1d56f2f0fe0
정승우 <sunjw0531@gmail.com>
2022-11-30 15:48:56 +0900

    .

--- 이진 탐색/이진 탐색(재귀 함수로 구현).py
+++ 이진 탐색/이진 탐색(재귀 함수로 구현).py
@@ -1,4 +1,24 @@
 # 이진 탐색은 배열 내부의 데이터가 정렬되어 있어야만 사용할 수 있는 알고리즘이다.
 # 데이터가 무작위일 때는 사용할 수 없지만, 이미 정렬되어 있다면 매우 빠르게 데이터를 찾을 수 있다는 특징이 있다.
 # 이진 탐색은 탐색 범위를 절반씩 좁혀가며 데이터를 탐색하는 특징이 있다.
+# 시간복잡도는 O(logN) 이다.
 
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid-1)
+    else:
+        return binary_search(array, target, mid+1, end)
+
+n, target = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n-1)
+if result == None:
+    print('원소가 존재하지 않습니다')
+else:
+    print(result + 1)
